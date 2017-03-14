8f829131cf608e1cdef14bf90f3c190c8bd00563
Manuel Espinosa V <manchax@gmail.com>
2017-03-14 09:48:37 -0600

    Using decimal instead of float for better precision

--- CalculatePi/Program.fs
+++ CalculatePi/Program.fs
@@ -2,19 +2,19 @@
 open System.Diagnostics
 
 let nilakantha n =
-    n * (n+1.0) * (n+2.0)
+    n * (n+1.0m) * (n+2.0m)
 
-let calcPi (steps:float) =
+let calcPi (steps:decimal) =
     let timer = new Stopwatch()
     timer.Start()
-    let mutable pi = 3.0
+    let mutable pi = 3.0m
     // use it to iterate between + and -
-    let mutable sign = 1.0
+    let mutable sign = 1m
 
     // start at 2 increment by 2 until desired steps
-    for n in 2.0 .. 2.0 .. steps do
-        pi <- pi + ( sign * 4.0/(nilakantha n) ) 
-        sign <- sign * -1.0
+    for n in 2.0m .. 2.0m .. steps do
+        pi <- pi + ( sign * 4.0m/(nilakantha n) ) 
+        sign <- sign * -1m
 
     printfn "Finished PI calc in %A. Steps %A" 
         timer.Elapsed steps
@@ -26,14 +26,28 @@ let machin = 4.0 * ((4.0 * Math.Atan(1.0/5.0)) - Math.Atan(1.0/239.0))
 [<EntryPoint>]
 let main argv =     
 
-    let pi5    = calcPi 5000.0
-    let pi10   = calcPi 10000.0
-    let pi100  = calcPi 100000.0
+    let pi5    = calcPi 5000m
+    let pi10   = calcPi 10000m
+    let pi100  = calcPi 100000m
+    let pi1M   = calcPi 1000000m
+    let pi10M  = calcPi 10000000m
+    let pi20M  = calcPi 20000000m
+    //let pi50M  = calcPi 50000000m
+    //let pi100M = calcPi 100000000m
+    //let pi200M = calcPi 200000000m
+    let pi10B  = calcPi 10000000000m
     //let piMax = calcPi System.Double.MaxValue-2.0
 
     printfn "  5K -> %.020f" pi5
     printfn " 10K -> %.020f" pi10
     printfn "100K -> %.020f" pi100
+    printfn "  1M -> %.020f" pi1M
+    printfn " 10M -> %.031f" pi10M
+    printfn " 20M -> %.031f" pi20M
+    //printfn " 50M -> %.031f" pi50M
+    //printfn "100M -> %.031f" pi100M
+    //printfn "200M -> %.031f" pi200M
+    printfn "  1B -> %.031f" pi10B
     
     printfn "Machin -> %f" machin
     //printfn "Max(int32) -> %.020f" piMax
