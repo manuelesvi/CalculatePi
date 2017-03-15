86d7ee640a0d24368a1224b9a33486287ef6ecdb
Manuel Espinosa V <manchax@gmail.com>
2017-03-14 18:42:06 -0600

    Display more digits for Machin

--- CalculatePi/Program.fs
+++ CalculatePi/Program.fs
@@ -24,8 +24,7 @@ let calcPi (steps:decimal) =
 let machin = 4.0 * ((4.0 * Math.Atan(1.0/5.0)) - Math.Atan(1.0/239.0))
 
 [<EntryPoint>]
-let main argv =     
-
+let main argv =
     let pi5    = calcPi 5000m
     let pi10   = calcPi 10000m
     let pi100  = calcPi 100000m
@@ -35,21 +34,20 @@ let main argv =
     //let pi50M  = calcPi 50000000m
     //let pi100M = calcPi 100000000m
     //let pi200M = calcPi 200000000m
-    let pi1B  = calcPi 1000000000m
-    //let piMax = calcPi System.Double.MaxValue-2.0
+    //let pi1B  = calcPi 1000000000m
 
-    printfn "  5K -> %.020f" pi5
-    printfn " 10K -> %.020f" pi10
-    printfn "100K -> %.020f" pi100
-    printfn "  1M -> %.020f" pi1M
-    printfn " 10M -> %.031f" pi10M
-    printfn " 20M -> %.031f" pi20M
+    printfn "    5K -> %.020f" pi5
+    printfn "   10K -> %.020f" pi10
+    printfn "  100K -> %.020f" pi100
+    printfn "    1M -> %.020f" pi1M
+    printfn "   10M -> %.031f" pi10M
+    printfn "   20M -> %.031f" pi20M
     //printfn " 50M -> %.031f" pi50M
     //printfn "100M -> %.031f" pi100M
     //printfn "200M -> %.031f" pi200M
-    printfn "  1B -> %.031f" pi1B
+    //printfn "  1B -> %.031f" pi1B
     
-    printfn "Machin -> %f" machin
+    printfn "Machin -> %.031f" machin
     //printfn "Max(int32) -> %.020f" piMax
     Console.ReadKey() |> ignore
     0 // return an integer exit code
