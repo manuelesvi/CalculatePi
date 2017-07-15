46c08e766b0cdde98745720b40a95c9d0ce6f5ca
Manuel <manchax@gmail.com>
2017-07-15 12:19:07 -0500

    Add files via upload

---
+++ hiperbola.m
@@ -0,0 +1,23 @@
+# grafica y analisis de -x^-10x 
+x=-20:0.1:10;
+y=(-1*x.^2)+(-10*x);
+c=[-1 -10 0];
+r=roots (c); #[-10 0]
+d=polyder (c); #[-2 -10]
+
+#primer derivada = pendiente curva
+z=d(1)*x+d(2);
+
+#recta tangente de un punto (a,b) = f'(a)*(x-a)+f(a)
+#primera raiz x = r(1) = -10, y = f(a) = 0
+t1=(d(1)*r(1)+d(2))*(x-r(1))+0;
+#segunda raiz x = r(2) = 0, y = f(a) = 0
+t2=(d(1)*r(2)+d(2))*(x-r(2))+0;
+
+#max y - centro
+max=roots(d); # -5
+maxy = polyval(c, max) * 2;
+miny = polyval(c, -20);
+
+plot(x,y, x,t1, x,t2, x,z,max*ones(1,251),maxy:-1:miny, r(1), 0, "marker", "*", r(2), 0, "marker", "*", max, 0, "marker", "*")
+legend("f(x) = -x^2-10x", "tangente raiz 1", "tangente raiz 2", "pendiente curva")
